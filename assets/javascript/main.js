4b38206812e9954167f46924fded1b6227226874
Carlos Molina <molina.carlos.dev@gmail.com>
2018-02-20 09:29:43 -0600

    Drag text added for players

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,29 +5,34 @@ function allowDrop(ev) {
 
 function drag(cardNumber) {
     elementID = document.getElementById(cardNumber);
-    console.log("dragging " + elementID.id);
+    // console.log(elementID);
+    // console.log("dragging " + elementID.id);
 }
 
 // PLAYER AREA DROPOFF
 var isplayerDiv = true
+var getPTxt = document.getElementById("dragPText")
 function playerDrop(ev) {
     ev.preventDefault();
-    console.log (document.getElementById("player"))
+    // console.log (document.getElementById("player"))
    if (isplayerDiv) {
+    getPTxt.setAttribute("style", "display:none");
     document.getElementById("player").appendChild(elementID);
-    console.log ("dropping " + elementID.id);
+    console.log ("dropping " + elementID.alt);
     isplayerDiv = false;
     }
 }
 
 //OPPONENT AREA DROPOFF
 var isOpponentDiv = true;
+var getOTxt = document.getElementById("dragOText")
 function oppDrop(ev) {
     ev.preventDefault();
-    console.log (document.getElementById("player"))
+    // console.log (document.getElementById("opponent"))
    if (isOpponentDiv) {
+    getOTxt.setAttribute("style", "display:none");
     document.getElementById("opponent").appendChild(elementID);
-    console.log ("dropping " + elementID.id);
+    console.log ("dropping " + elementID.alt);
     isOpponentDiv = false;
     }
 }
