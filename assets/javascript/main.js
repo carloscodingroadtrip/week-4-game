4503be9b51d999b0d9f46094cf01a96570ef8671
Carlos Molina <molina.carlos.dev@gmail.com>
2018-02-20 11:23:40 -0600

    Life span add

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,4 @@
+
 var elementID;
 function allowDrop(ev) {
     ev.preventDefault();
@@ -6,17 +7,18 @@ function allowDrop(ev) {
 function drag(cardNumber) {
     elementID = document.getElementById(cardNumber);
     // console.log(elementID);
-    // console.log("dragging " + elementID.alt);
 }
 
 // PLAYER AREA DROPOFF
 var isplayerDiv = true
 var getPTxt = document.getElementById("dragPText")
+var playerLife = document.getElementById("playerSpan")
 function playerDrop(ev) {
     ev.preventDefault();
     // console.log (document.getElementById("player"))
    if (isplayerDiv) {
     getPTxt.setAttribute("style", "display:none");
+    playerLife.setAttribute("style", "visibility: visible");
     document.getElementById("player").appendChild(elementID);
     console.log ("dropping " + elementID.alt);
     isplayerDiv = false;
@@ -26,13 +28,27 @@ function playerDrop(ev) {
 //OPPONENT AREA DROPOFF
 var isOpponentDiv = true;
 var getOTxt = document.getElementById("dragOText")
+var opponentLife = document.getElementById("opponentSpan")
 function oppDrop(ev) {
     ev.preventDefault();
     // console.log (document.getElementById("opponent"))
    if (isOpponentDiv) {
     getOTxt.setAttribute("style", "display:none");
+    opponentLife.setAttribute("style", "visibility: visible");
     document.getElementById("opponent").appendChild(elementID);
     console.log ("dropping " + elementID.alt);
     isOpponentDiv = false;
     }
 }
+
+function attackGenerator(playerHP, opponentHP) {
+    // if PLAYER HP > OPPONENT HP {
+        // have the opponent attacks be greater
+    // else
+    //     have the player attacks be greater value
+
+    //
+
+}
+
+
