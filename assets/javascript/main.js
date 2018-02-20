691f5de445e95cd1a23e55ebae7f9364b3e1c540
Carlos Molina <molina.carlos.dev@gmail.com>
2018-02-20 00:55:22 -0600

    Draggable is fixed

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,33 @@
+var elementID;
+function allowDrop(ev) {
+    ev.preventDefault();
+}
 
+function drag(cardNumber) {
+    elementID = document.getElementById(cardNumber);
+    console.log("dragging " + elementID.id);
+}
+
+// PLAYER AREA DROPOFF
+var isplayerDiv = true
+function playerDrop(ev) {
+    ev.preventDefault();
+    console.log (document.getElementById("player"))
+   if (isplayerDiv) {
+    document.getElementById("player").appendChild(elementID);
+    console.log ("dropping " + elementID.id);
+    isplayerDiv = false;
+    }
+}
+
+//OPPONENT AREA DROPOFF
+var isOpponentDiv = true;
+function oppDrop(ev) {
+    ev.preventDefault();
+    console.log (document.getElementById("player"))
+   if (isOpponentDiv) {
+    document.getElementById("opponent").appendChild(elementID);
+    console.log ("dropping " + elementID.id);
+    isOpponentDiv = false;
+    }
+}
