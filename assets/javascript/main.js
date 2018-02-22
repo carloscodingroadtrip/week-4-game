1ec69cdc26b6bfa272145eed6ff188b1f64f2fbe
Carlos Molina <molina.carlos.dev@gmail.com>
2018-02-21 22:23:00 -0600

    Span life for both characters is DONE

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -13,76 +13,81 @@ var playerLife = document.getElementById("playerSpan")
 var opponentLife = document.getElementById("opponentSpan")
 var attackBtn = document.getElementById("attackBtn");
 
-function randomHP() {
+(function randomHP() {
     for (i = 1; i < 5; i++) {
         hpLife = Math.floor(Math.random() * 50 ) + 120; //Random HP Health Points
-        console.log('random HP for char ' + hpLife);
         $("#char" + i).text("HP: " + hpLife)[0]; // It means JQuery index
         $(".sel" +i ).attr("charLife", hpLife)[0]; // I CAN USE the [0] to do many many things
       }
-}
-randomHP();
+})()
 
 // PLAYER AREA DROPOFF
-var isplayerDiv = true
+var isPlayerDivEmpty = true;
+var playerInitHP;
 function playerDrop(ev) { //is equal only to my img  === elementID
     ev.preventDefault();
+    document.getElementById("player").removeAttribute("ondrop");
     var playerParent = elementID.parentNode; //get the parent of the img (elementID) being
-   if (isplayerDiv) {
+    playerInitHP = parseInt( $(elementID).attr("charLife"));
+    console.log('Starting span life of player is ' + parseInt(  $(elementID).attr("charLife")    ) );
+   if (isPlayerDivEmpty) {
     getPTxt.setAttribute("style", "display:none"); //hide the Drag message instruction
     playerLife.setAttribute("style", "visibility: visible"); //Display the character life span
     playerParent.setAttribute("style", "visibility: hidden");  //hide the parent HTML
-    document.getElementById("player").appendChild(elementID); //append the draggable character to my target
+    document.getElementById("player").appendChild(elementID); //append the draggable character
+    document.getElementById("playerSpan").innerHTML = playerInitHP;
     elementID.setAttribute("style","margin-left:50px"); //adjust the character margin
-    console.log ("dropping " + elementID.alt);
-    isplayerDiv = false;
+    // console.log ("dropping " + elementID.alt);
+    isPlayerDivEmpty = false;
     }
+    return playerInitHP; //I am returning the starting HP Life for the PLAYER
 }
 
 //OPPONENT AREA DROPOFF
 var isOpponentDiv = true;
+var opponentInitHP;
+
 function oppDrop(ev) {
+    document.getElementById("opponent").removeAttribute("ondrop");
     ev.preventDefault();
-    console.log(  $(elementID).attr("charLife")     );
-    var opponentParent = elementID.parentNode;
+    opponentInitHP = parseInt( $(elementID).attr("charLife"));
+    console.log( 'Starting span life of opponent is ' + opponentInitHP );
+    var opponentParent = elementID.parentNode; //get the parent of whatever
    if (isOpponentDiv) {
     getOTxt.setAttribute("style", "display:none");
     opponentLife.setAttribute("style", "visibility: visible");
     attackBtn.setAttribute("style", "visibility: visible");
     document.getElementById("opponent").appendChild(elementID);
+    document.getElementById("opponentSpan").innerHTML = opponentInitHP;
     elementID.setAttribute("style","margin-left:25px")
     opponentParent.setAttribute("style", "visibility: hidden");
-    console.log ("dropping " + elementID.alt);
+    // console.log ("dropping " + elementID.alt);
     isOpponentDiv = false;
     }
+    return opponentInitHP; //I am returning the starting HP Life for the OPPONENT
 }
 
 var playerAttack, enemyAttack;
-
 //Here is my object to use for attacks
 var character = {
-    punch: 5,
-    //  Math.floor(Math.random()*6) + 5, // 5 -10
-    kick:  8,
-    //   Math.floor(Math.random()* 10 ) + 4,
-    combo: 15,
-    // Math.floor(Math.random()* 12 ) + 7,
+    punch: 12,  //0
+    kick:  16,  //1
+    combo: 20,  //2
 }
 
-function goGetMeAPlayerAttack() {
+//Get me an attack
+function getAtackMode() {
     var hereisYourPlayerAttack = Math.floor(Math.random()* 3); // 0 -2
     return hereisYourPlayerAttack;
 }
 
-function goGetMeAnEnemyAttack() {
-    var hereisYourEnemyAttack = Math.floor(Math.random()* 3); // 0 -2
-    return hereisYourEnemyAttack;
-}
-
+//GLOBAL VARIABLES DECLARATION
+var attackModeForPlayer, attackModeForEnemy;
+var newHPValuePlayer,newHPValueEnemy, ongoingOpponentHP, ongoingPlayerHP;
+var firstClickRead = true; //This is the flag for the first time
 $("#attack").on("click", function() {
-    playerAttack = goGetMeAPlayerAttack(); //0
-    enemyAttack = goGetMeAnEnemyAttack(); //1
-    var attackModeForPlayer, attackModeForEnemy;
+    playerAttack = getAtackMode(); //0
+    enemyAttack = getAtackMode(); //1
    switch (playerAttack) {
      case 0:
         attackModeForPlayer = character.punch;
@@ -97,8 +102,6 @@ $("#attack").on("click", function() {
        console.log('The player attack was a combo : ' + attackModeForPlayer);
        break;
    }
-
-
    switch (enemyAttack){
      case 0:
         attackModeForEnemy = character.punch;
@@ -113,27 +116,33 @@ $("#attack").on("click", function() {
        console.log('The ENEMY attack was a combo : ' + attackModeForEnemy);
        break;
    }
+   //Fighting function
+   (function() {
+       console.log('Was this my first click ? ' + firstClickRead);
+       if (firstClickRead) {
+        newHPValuePlayer = playerInitHP - attackModeForEnemy;  /// 100 - 5  = 95 newHPValuePlayer
+        newHPValueEnemy = opponentInitHP - attackModeForPlayer; /// 120 - 15  = 105 newHPValueOpponent
+        ongoingPlayerHP = newHPValuePlayer; //95 newHPValuePlayer ONGOING
+        ongoingOpponentHP = newHPValueEnemy; //105 newHPValueOpponent ONGOING
+        document.getElementById("playerSpan").innerHTML = ongoingPlayerHP;
+        document.getElementById("opponentSpan").innerHTML = ongoingOpponentHP;
+        console.log('Player HP after 1st click ...  ' + ongoingPlayerHP);
+        console.log('Opponent HP after 1st click ...  ' + ongoingOpponentHP);
+        firstClickRead = false;
+        console.log('First click has been captured');
+        console.log('-------------DONE -------------------');
+       } else {
+        console.log('---- Here comes second to infinity click ----');
+
+        //Record the damages to both HPs
+        ongoingPlayerHP -= attackModeForEnemy;
+        ongoingOpponentHP -= attackModeForPlayer;
+        document.getElementById("playerSpan").innerHTML = ongoingPlayerHP;
+        document.getElementById("opponentSpan").innerHTML = ongoingOpponentHP;
+        console.log('Next round Player HP ' + ongoingPlayerHP);
+        console.log('Next round Opponent HP ' + ongoingOpponentHP);
+       }
+    })();
    console.log('--------------------------------');
+   return attackModeForPlayer, attackModeForEnemy;
  });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
